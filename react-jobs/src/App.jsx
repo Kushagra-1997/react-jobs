8344a6e5bb60eb36bd8c59b5c503858b6c7403fd
Kushagra-1997 <kush.rastogi18@gmail.com>
2024-07-05 07:31:52 +0530

    created routes for every link using react-router-dom

--- react-jobs/src/App.jsx
+++ react-jobs/src/App.jsx
@@ -1,20 +1,32 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListings from './components/JobListings'
-import ViewAllJobs from './components/ViewAllJobs'
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import MainLayout from './Layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}> 
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='/*' element={<NotFoundPage />} />
+    </Route>
+  )
+)
 
 const App = () => {
   return (
     <>
-    <Navbar />
-    <Hero />
-    <HomeCards />
-    <JobListings />
-    <ViewAllJobs /> 
+      <RouterProvider router={router} />
     </>
   )
 }
 
-export default App;
+export default App
---
+++ react-jobs/src/pages/JobsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function JobsPage() {
+  return (
+    <>
+      <h1>JobsPage</h1>
+    </>
+  )
+}
+
+export default JobsPage
