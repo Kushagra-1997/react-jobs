5da72e44c5e0206b2f920d897719179d406b4a43
Kushagra-1997 <kush.rastogi18@gmail.com>
2024-07-08 07:35:12 +0530

    added functionality for add new Job post

--- react-jobs/src/App.jsx
+++ react-jobs/src/App.jsx
@@ -9,19 +9,39 @@ import HomePage from './pages/HomePage';
 import MainLayout from './Layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
-
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<MainLayout />}> 
-      <Route index element={<HomePage />} />
-      <Route path='/jobs' element={<JobsPage />} />
-      <Route path='/*' element={<NotFoundPage />} />
-    </Route>
-  )
-)
+import JobPage, {jobLoader} from './pages/JobPage'
+import AddJobPage from './pages/AddJobPage'
 
 const App = () => {
+
+  const addJob = async (newJob) =>{
+    const resp = await fetch('/api/jobs',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  }
+
+  // delete Job
+  const deleteJob = async(id) =>{
+    console.log('delete', id);
+  }
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout />}> 
+        <Route index element={<HomePage />} />
+        <Route path='/jobs' element={<JobsPage />} />
+        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob} />} loader={jobLoader} />
+        <Route path='/*' element={<NotFoundPage />} />
+      </Route>
+    )
+  )  
+
   return (
     <>
       <RouterProvider router={router} />
--- react-jobs/src/pages/JobsPage.jsx
+++ react-jobs/src/pages/JobsPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import JobListsings from '../components/JobListings'
 
 function JobsPage() {
   return (
-    <>
-      <h1>JobsPage</h1>
-    </>
+    <section className='bg-blue-50 px-4 py-6'>
+      <JobListsings />
+    </section>
   )
 }
 
